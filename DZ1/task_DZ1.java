6d5ec498945b0277c21b4f80959dac2bc4700d91
gugkas <gugkas@yandex.ru>
2023-07-17 16:08:37 +0300

    task_DZ1

---
+++ DZ1/task_DZ1.java
@@ -0,0 +1,39 @@
+package DZ1;
+
+public class task_DZ1 {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Барсик ", 5);
+        System.out.println("Мяу, меня зовут: " + Cat.getName() + "мне " + Cat.age + " лет");
+
+    }
+}
+
+class Cat {
+
+    public static String getAge;
+    private static String name;
+    static int age;
+
+    public Cat(String name, int age) {
+        Cat.name = name;
+        Cat.age = age;
+    }
+
+    public void setName(String name) {
+        Cat.name = name;
+    }
+
+    public void setAge(int age) {
+        Cat.age = age;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+
+}
