2c1da0461d4247801b7ba415c97d4e1db2bd0de7
gugkas <gugkas@yandex.ru>
2023-07-28 13:24:27 +0300

    cat commit

--- DZ1/Cat/task_DZ1.java
+++ DZ1/Cat/task_DZ1.java
@@ -1,37 +1,44 @@
 package DZ1.Cat;
 
 public class task_DZ1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         new Cat("Барсик ", 5);
         System.out.println("Мяу, меня зовут: " + Cat.getName() + "мне " + Cat.age + " лет");
 
     }
 }
 
-class Cat {
+class Cat 
+{
 
     public static String getAge;
     private static String name;
     static int age;
 
-    public Cat(String name, int age) {
+    public Cat(String name, int age)
+    {
         Cat.name = name;
         Cat.age = age;
     }
 
-    public void setName(String name) {
+    public void setName(String name) 
+    {
         Cat.name = name;
     }
 
-    public void setAge(int age) {
+    public void setAge(int age) 
+    {
         Cat.age = age;
     }
 
-    public static String getName() {
+    public static String getName() 
+    {
         return name;
     }
 
-    public double getAge() {
+    public double getAge() 
+    {
         return age;
     }
 
