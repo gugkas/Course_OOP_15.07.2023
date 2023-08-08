771a93ca8040f20d19167e800cb9d9c9408cad03
gugkas <gugkas@yandex.ru>
2023-08-08 13:34:25 +0300

    CalcPres

--- DZ5/CalculatorView.java
+++ DZ5/CalculatorView.java
@@ -2,4 +2,21 @@ package DZ5;
 
 public class CalculatorView {
 
+    public void showWelcomeMessage() {
+    }
+
+    public String getUserOperation() {
+        return null;
+    }
+
+    public void showError(String string) {
+    }
+
+    public double getUserNumber(String string) {
+        return 0;
+    }
+
+    public void getResult(double add) {
+    }
+
 }
