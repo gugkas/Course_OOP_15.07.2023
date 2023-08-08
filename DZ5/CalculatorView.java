f4b35b938202e9634e450b6b54e99593375260c0
gugkas <gugkas@yandex.ru>
2023-08-08 13:39:57 +0300

    CalcView

--- DZ5/CalculatorView.java
+++ DZ5/CalculatorView.java
@@ -1,22 +1,36 @@
 package DZ5;
-
+import java.util.Scanner;
 public class CalculatorView {
-
+    private static final String message = null;
+    private static final String name = null;
+    private static final String result = null;
+    private final Scanner scanner = new Scanner(System.in);
     public void showWelcomeMessage() {
+        System.out.println("Добро пожаловать в калькулятор!");
     }
 
     public String getUserOperation() {
-        return null;
+        System.out.println("Введите знак действия (+, -, *, /):");
+        return scanner.nextLine();
     }
 
     public void showError(String string) {
-    }
+    
+        System.out.println("Ошибка: " + message);}
 
     public double getUserNumber(String string) {
-        return 0;
+        System.out.println("Введите " + name + " число:");
+        while (true) {
+            try {
+                return Double.parseDouble(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Неправильное число");
+            }
+        }
     }
 
     public void getResult(double add) {
-    }
+        System.out.println("Результат: " + result);
+     }
 
 }
