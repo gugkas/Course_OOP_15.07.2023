edd68010a163238bfcbd639eeb6f7431ea1a4320
gugkas <gugkas@yandex.ru>
2023-08-08 13:29:29 +0300

    DZ5

---
+++ DZ5/CalculatorPresenter.java
@@ -0,0 +1,11 @@
+package DZ5;
+
+public class CalculatorPresenter {
+
+    public CalculatorPresenter(CalculatorModel model, CalculatorView view) {
+    }
+
+    public void onAddButtonClicked() {
+    }
+
+}
---
+++ DZ5/CalculatorView.java
@@ -0,0 +1,5 @@
+package DZ5;
+
+public class CalculatorView {
+
+}
