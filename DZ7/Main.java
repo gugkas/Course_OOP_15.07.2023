461f7b53fb03c93294e21258e6f353bb0da93c9e
gugkas <gugkas@yandex.ru>
2023-08-14 20:07:47 +0300

    DZ7

---
+++ DZ7/Main.java
@@ -0,0 +1,10 @@
+package examples.calculator;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ICalculableFactory calculableFactory = new CalculableFactory();
+        ViewCalculator view = new ViewCalculator(calculableFactory);
+        view.run();
+    }
+}
